81cc78a9d7c6120da9ad6e8fc8cb6a2a6c93223a
Kujtim Durguti <kujtim009@gmail.com>
2020-11-13 03:40:48 +0100

    attt

--- test_connection.py
+++ test_connection.py
@@ -5,21 +5,3 @@ import pandas
 import pyodbc
 import pandas as pd
 from sqlalchemy import create_engine, event
-
-DRIVER = "ODBC Driver 17 for SQL Server"
-DSN = "208.118.231.180:21201"
-DB = "InsertTool"
-TABLE = "CBD_Sample_test"
-sql_username = "ITRemoteUser"
-sql_password = "Remote009I!"
-# conn_executemany = create_engine(
-#     f"mssql+pyodbc://ITRemoteUser:Remote009I!@{DSN}/{DB}?driver={DRIVER}", fast_executemany=True
-quotedLocalPc = urllib.parse.quote_plus(
-    "DRIVER={ODBC Driver 17 for SQL Server};SERVER=208.118.231.180,21201;UID=ITRemoteUser;PWD=Remote009I!;DATABASE=InsertTool;Trusted_Connection=no;")
-
-# quoted = quote_plus(conn)
-new_con = 'mssql+pyodbc:///?odbc_connect={}'.format(quotedLocalPc)
-# engine = create_engine(new_con)
-engine = create_engine(new_con)
-
-engine.execute('select *from Api_Fgx_log')
