87e5939abd0c664e4a2f252f3e5c98e449903750
Kujtim Durguti <kujtim009@gmail.com>
2020-11-13 03:23:38 +0100

    test1

--- test_connection.py
+++ test_connection.py
@@ -10,10 +10,16 @@ DRIVER = "ODBC Driver 17 for SQL Server"
 DSN = "208.118.231.180:21201"
 DB = "InsertTool"
 TABLE = "CBD_Sample_test"
+sql_username = "ITRemoteUser"
+sql_password = "Remote009I!"
+# conn_executemany = create_engine(
+#     f"mssql+pyodbc://ITRemoteUser:Remote009I!@{DSN}/{DB}?driver={DRIVER}", fast_executemany=True
+quotedLocalPc = urllib.parse.quote_plus(
+    "DRIVER={SQL Server};SERVER=208.118.231.180,21201;UID=ITRemoteUser;PWD=Remote009I!;DATABASE=InsertTool;Trusted_Connection=no;")
 
+# quoted = quote_plus(conn)
+new_con = 'mssql+pyodbc:///?odbc_connect={}'.format(quotedLocalPc)
+# engine = create_engine(new_con)
+engine = create_engine(new_con)
 
-conn_executemany = create_engine(
-    f"mssql+pyodbc://ITRemoteUser:Remote009I!@{DSN}/{DB}?driver={DRIVER}", fast_executemany=True
-)
-
-conn_executemany.execute('select *from Api_Fgx_log')
+engine.execute('select *from Api_Fgx_log')
